7448656eca39370e4e96bb34f55e87cb0102fd52
yash7xm <yash.7xm@gmail.com>
2024-10-18 19:32:35 +0530

    cors added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 	"syscall"
 
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 	"github.com/yash7xm/Rule_Engine_with_AST/cmd/routes"
 	db "github.com/yash7xm/Rule_Engine_with_AST/internal/database"
 )
@@ -24,14 +25,19 @@ func main() {
 	db.InitDB()
 	defer db.DB.Close() // Close the DB connection when the application shuts down
 
-	// Run database migrations
-	// err = db.RunMigrations()
-	// if err != nil {
-	// 	log.Fatalf("Error running migrations: %v", err)
-	// }
-
 	// Set up the server with routes
 	router := routes.NewRouter()
+
+	// Enable CORS
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Content-Type"},
+		AllowCredentials: true,
+	})
+
+	// Wrap the router with the CORS handler
+	handler := corsHandler.Handler(router)
 
 	// Capture system signals to handle graceful shutdown
 	stopChan := make(chan os.Signal, 1)
@@ -41,7 +47,7 @@ func main() {
 	fmt.Printf("Server is running on port %s...\n", port)
 
 	go func() {
-		if err := http.ListenAndServe(port, router); err != nil {
+		if err := http.ListenAndServe(port, handler); err != nil {
 			log.Fatalf("Error starting server: %s\n", err)
 		}
 	}()
